455a1cd7c3bdb50ea3debdcf103576e9dbd8c110
punkyoon <cryptography@konkuk.ac.kr>
2018-06-06 11:01:05 +0000

    sensor driver done

--- driver/sensor_driver.c
+++ driver/sensor_driver.c
@@ -4,6 +4,15 @@
 #include <linux/fs.h>
 #include <linux/gpio.h>
 #include <linux/time.h>
+#include <linux/cdev.h>
+#include <linux/fcntl.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+
+#include <asm/uaccess.h>
+
+#include "sensor_driver.h"
 
 #define DEV_NAME "sensor_driver"
 
@@ -19,13 +28,16 @@
 
 MODULE_LICENSE("GPL");
 
-static int ultrasonic_read(void)
+static struct cdev *cd_cdev;
+dev_t dev_num;
+
+static unsigned long ultrasonic_read(void)
 {
     struct timeval start_time, end_time;
     unsigned long start, end, travel;
 
-    // Cannot use floating point in kernel driver
-    // distance = travel * 0.01715 (cm)
+    // Cannot use floating porint in kernel driver
+    // distance = travel * 0.01715 or travel / 58 (cm)
 
     gpio_set_value(TRIG, 0);
     udelay(2);
@@ -34,21 +46,21 @@ static int ultrasonic_read(void)
     udelay(10);
     gpio_set_value(TRIG, 0);
 
-    // Measuring excution time
+    // Measuring execution time
     while(gpio_get_value(ECHO) == 0);
     do_gettimeofday(&start_time);
 
-    while(gpio_get_value(ECHO == 1));
+    while(gpio_get_value(ECHO) == 1);
     do_gettimeofday(&end_time);
 
+    // Convert to microsecond
     start = (unsigned long)start_time.tv_sec*1000000 + (unsigned long)start_time.tv_usec;
     end = (unsigned long)end_time.tv_sec*1000000 + (unsigned long)end_time.tv_usec;
+    travel = end - start;    // execution time (microsecond)
 
-    travel = end - start;    // microsecond
     return travel;
 }
 
-/*
 static int sensor_driver_open(struct inode *inode, struct file *file)
 {
     printk("[SENSOR_DRIVER] Open\n");
@@ -61,16 +73,56 @@ static int sensor_driver_release(struct inode *inode, struct file *file)
     return 0;
 }
 
+static long sensor_driver_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+    printk("[SENSOR_DRIVER] IOCTL\n");
+
+    switch(cmd)
+    {
+        // Get sensor data
+        case SENSOR_SONIC:
+            return (long)ultrasonic_read();
+
+        // Set Red LED
+        case SENSOR_R_LED:
+            gpio_set_value(G_LED, 0);
+            gpio_set_value(R_LED, 1);
+            break;
+
+        // Set Green LED
+        case SENSOR_G_LED:
+            gpio_set_value(R_LED, 0);
+            gpio_set_value(G_LED, 1);
+            break;
+
+        // Exit Application
+        case SENSOR_CLOSE:
+            gpio_set_value(R_LED, 0);
+            gpio_set_value(G_LED, 0);
+
+        default:
+            return 0;
+    }
+
+    return 1;
+}
+
 struct file_operations sensor_driver_fops =
 {
     .open = sensor_driver_open,
-    .release = sensor_driver_release
+    .release = sensor_driver_release,
+    .unlocked_ioctl = sensor_driver_ioctl
 };
-*/
 
 static int __init sensor_driver_init(void)
 {
     printk("[SENSOR_DRIVER] Init\n");
+
+    // Init dev
+    cd_cdev = cdev_alloc();
+    alloc_chrdev_region(&dev_num, 0, 1, DEV_NAME);
+    cdev_init(cd_cdev, &sensor_driver_fops);
+    cdev_add(cd_cdev, dev_num, 1);
 
     // Init GPIO
     gpio_request_one(TRIG, GPIOF_OUT_INIT_LOW, "TRIG");
@@ -93,6 +145,10 @@ static void __exit sensor_driver_exit(void)
     gpio_free(ECHO);
     gpio_free(R_LED);
     gpio_free(G_LED);
+
+    // Free dev
+    cdev_del(cd_cdev);
+    unregister_chrdev_region(dev_num, 1);
 }
 
 module_init(sensor_driver_init);
